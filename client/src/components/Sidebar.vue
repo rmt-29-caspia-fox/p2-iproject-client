cb4b843c853377a3bc4260a48679ee323efa6b42
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-11-09 14:27:26 +0700

    login and navigation guard

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push("/admin/login");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="col-2">
+    <nav class="navbar-sidebar bg-linier-1 text-light" id="nav">
+      <div class="nav-items">
+
+        <!-- <a class="item-list mb-2">
+          <div class="icons me-1">
+            <i class="fas fa-clock"></i>
+          </div>
+          <span></span>
+        </a> -->
+        <div class="border-bottom border-top">
+            <a class="logout" @click.prevent="logout">
+              <div class="icons me-1">
+                <i class="fas fa-sign-out-alt"></i>
+              </div>
+              <span class="text-light">Logout</span>
+            </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ client/src/views/AdminTableView.vue
@@ -0,0 +1,16 @@
+<script>
+import Sidebar from '../components/Sidebar.vue';
+
+export default{
+    components:{
+        Sidebar
+    }
+}
+</script>
+
+<template>
+    <div class="row">
+        <Sidebar/>
+        
+    </div>
+</template>
