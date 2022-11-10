824cd9bc03b429d213aabc63f51be01340da4abe
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-11-10 09:34:11 +0700

    deployed: https://iproject-carwosssh.web.app

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'https://iproject-server-caspia.herokuapp.com',
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
   });
 
 export default instance
