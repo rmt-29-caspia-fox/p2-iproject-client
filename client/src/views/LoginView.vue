17c2c9b0f0ed21e039be9eeccde4a897644929f2
Jacob <silaenjacob@gmail.com>
2022-11-10 12:34:06 +0800

    Deployed 1

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -42,7 +42,7 @@ export default ({
                 </div>
                 <div class="w-100">
                   <p class="social-media d-flex justify-content-end">
-                    <a href="https://discord.com/api/oauth2/authorize?client_id=1039609972046168114&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Flogin&response_type=code&scope=identify%20email"
+                    <a href="https://discord.com/api/oauth2/authorize?client_id=1039609972046168114&redirect_uri=https%3A%2F%2Fdbuilder-iproject.web.app%2Flogin&response_type=code&scope=identify%20email"
                       class="social-icon d-flex align-items-center justify-content-center">
                       <font-awesome-icon icon="fa-brands fa-discord" @click.prevent />
                     </a>
