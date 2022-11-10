1eb7f5dd4ca712e76a13e864b0b9f53601c59605
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-11-10 08:16:30 +0700

    add google sign in button

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
 import Paginate from "vuejs-paginate-next";
+import vue3GoogleLogin from 'vue3-google-login'
 
 import App from "./App.vue";
 import router from "./router";
@@ -15,6 +16,10 @@ const pinia = createPinia();
 pinia.use(({ store }) => {
   store.router = markRaw(router);
 });
+
+app.use(vue3GoogleLogin, {
+  clientId: '96399291446-iau9r6vu6pkcleush8btvh74t2s43jk5.apps.googleusercontent.com'
+})
 
 app.use(Paginate)
 app.use(pinia);
