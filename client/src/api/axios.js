3b10790592ecc696034395400ce72a00dc08579a
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-11-10 09:13:23 +0700

    ready to deploy

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
+    baseURL: 'https://iproject-server-caspia.herokuapp.com',
     baseURL: 'http://localhost:3000',
   });
 
--- client/src/api/socketio.js
+++ client/src/api/socketio.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3030")
+const socket = io("https://iproject-server-caspia.herokuapp.com/")
 
 export default socket
